1910551397837ad46531bda2d6f753e3e6e23886
Tom Graham <tom@sirwhite.com>
2019-01-22 16:54:59 +1100

    Initial release.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# firetime
-Chrome extension that displays a moment.js relative time for timestamps when viewing data in the Firebase console.
+# FireTime
+
+FireTime is a chrome extension that displays a moment.js relative time for timestamps when viewing data in the Firebase console.
+
+I got sick of copy and pasting firebase time stamps into unix epoch time converts (after dividing the number by 1000). This extension solves that problem.
+
+![FireTime in action](screenshot.png)
+
+FireTime is in no way endorsed by Firebase, Google, or the Moment.js team. Use it at your own risk.
---
+++ content.js
@@ -0,0 +1,22 @@
+const MAX_TIME = 32503679999; // 2999/12/31
+const MIN_TIME = 946684800; // 2000/01/01
+
+var observer = new MutationObserver(function(mutations) {
+  var inputs = document.getElementsByClassName('valueedit');
+  for (var i = 0, l = inputs.length; i < l; i++) {
+    var node = inputs[i];
+    var time = Number(node.value) / 1000;
+    if (isNaN(time) || time < MIN_TIME || time > MAX_TIME) {
+      continue; // probably not a date
+    }
+    if (node.nextSibling && node.nextSibling.className == "firetime") {
+      continue;
+    }
+    var fireTime = document.createElement("span");
+    fireTime.className = "firetime";
+    fireTime.appendChild(document.createTextNode(moment.unix(time).fromNow()));
+    node.parentNode.insertBefore(fireTime, node.nextSibling);
+  }
+});
+
+observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
